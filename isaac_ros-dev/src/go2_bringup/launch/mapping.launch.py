b27734504f32fd89c9e152e078a528b6c0b2b2de
jcastel358 <jcass358@gmail.com>
2025-05-08 15:03:41 -0400

    minor changed to mapping script to start fresh

--- isaac_ros-dev/src/go2_bringup/launch/mapping.launch.py
+++ isaac_ros-dev/src/go2_bringup/launch/mapping.launch.py
@@ -86,6 +86,7 @@ def generate_launch_description():
                     'publish_tf': True,
                     'map_frame_id': 'map',
                     'odom_frame_id': 'odom',
+                    "delete_db_on_start": True,
                     'use_sim_time': LaunchConfiguration('use_sim_time')}],
         remappings=[('scan_cloud', '/pointcloud'),
                     ('odom', '/odom')],
